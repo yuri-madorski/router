62262d7da85967c86143b3353eef783b5802db72
Yuri <110467594+yuri-madorski@users.noreply.github.com>
2023-12-04 19:29:01 +0000

    Update

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -12,6 +12,7 @@ export class Node {
   lon: number;
   next_nodes: {
     id: number;
+    path?: number[];
     distance: number;
     tags: any;
   }[]
@@ -88,12 +89,15 @@ export class MapComponent implements OnInit {
   terminal_stops = [];
   terminal_stops_nodes = [];
   terminal_stops_on_map: any[] = [];
+  depots_stops_on_map: any[] = [];
   sel_depot: any;
   sel_depot_on_map: any;
   desired_routes: {
     start_node?: number;
+    start_node_on_map?: any;
     start_stop: any;
     end_node?: number;
+    end_node_on_map?: any;
     end_stop: any;
     distance: number;
     time: number;
@@ -103,7 +107,7 @@ export class MapComponent implements OnInit {
   rows: DHRow[] = [];
   produceXLSX() {
     this.rows = [];
-    for (let dr of this.desired_routes) {
+    for (let dr of this.desired_routes.filter(el => el.distance <= this.max_distance)) {
       this.rows.push({
         "Origin Stop Id": dr.start_stop.id,
         "Destination Stop Id": dr.end_stop.id,
@@ -153,8 +157,11 @@ export class MapComponent implements OnInit {
             this.terminal_stops.push(stop);
         }
         console.log('terminal_stops', this.terminal_stops);
+        for (let ts of this.depots) {
+          this.depots_stops_on_map.push(L.circle([ts.lat, ts.long], { color: 'red' }).bindTooltip(ts.id).addTo(this.map));
+        }
         for (let ts of this.terminal_stops) {
-          this.terminal_stops_on_map.push(L.circle([ts.lat, ts.long]).addTo(this.map));
+          this.terminal_stops_on_map.push(L.circle([ts.lat, ts.long]).bindTooltip(ts.id).addTo(this.map));
           for (let depot of this.depots) {
             if (depot.id !== ts.id) {
               this.desired_routes.push({
@@ -286,9 +293,13 @@ export class MapComponent implements OnInit {
         }
       }
       for (let ts of this.terminal_stops) {
-        //let ts_node = this.findClosest(ts.lat, ts.long);
-        //ts['node'] = ts_node;
         this.terminal_stops_nodes.push(ts['node']);
+        L.circle([this.nodes_index[ts['node']].lat, this.nodes_index[ts['node']].lon], { radius: 1, opacity: .5, color: 'green' }).addTo(this.map);
+        L.polyline([[ts.lat, ts.long], [this.nodes_index[ts['node']].lat, this.nodes_index[ts['node']].lon]]).addTo(this.map);
+      }
+      for (let ts of this.depots) {
+        L.circle([this.nodes_index[ts['node']].lat, this.nodes_index[ts['node']].lon], { radius: 1, opacity: .5, color: 'red' }).addTo(this.map);
+        L.polyline([[ts.lat, ts.long], [this.nodes_index[ts['node']].lat, this.nodes_index[ts['node']].lon]]).addTo(this.map);
       }
       console.log('terminal_stops_nodes', this.terminal_stops_nodes);
       for (let dr of this.desired_routes) {
@@ -299,12 +310,12 @@ export class MapComponent implements OnInit {
         if (el.type == 'way') {
           this.ways.push({ id: el.id, nodes: el.nodes, tags: el.tags });
           for (let pair of this.pairwise(el.nodes)) {
-            this.nodes_index[pair[0]].next_nodes.push({ id: pair[1], distance: 0, tags: el.tags });
+            this.nodes_index[pair[0]].next_nodes.push({ id: pair[1], distance: 0, tags: el.tags, path: [] });
             let other_dir = true;
             if (el.tags['junction'] && el.tags['junction'] == 'roundabout') other_dir = false;
             if (el.tags['oneway'] == 'yes' && el.tags['oneway:psv'] !== 'no' && el.tags['oneway:bus'] !== 'no') other_dir = false;
             if (other_dir) {
-              this.nodes_index[pair[1]].next_nodes.push({ id: pair[0], distance: 0, tags: el.tags });
+              this.nodes_index[pair[1]].next_nodes.push({ id: pair[0], distance: 0, tags: el.tags, path: [] });
             }
           }
         }
@@ -319,9 +330,44 @@ export class MapComponent implements OnInit {
         }
       }
       console.log('distances calculated');
+      /*let node_ids_to_delete = [];
+      let node_ids = [];
+      let terminal_and_depots = [...this.terminal_stops_nodes, ...this.depots.map(el => el.node)];
+      for (let node_id in this.nodes_index) {
+        node_ids.push(node_id);
+        let node = this.nodes_index[node_id];
+        for (let nn of node.next_nodes) {
+          let n1 = this.nodes_index[nn.id];
+          let n1_nns = n1.next_nodes.filter(el => el.id != node.id && nn.path.indexOf(el.id) == -1);
+          while (n1_nns.length == 1 && terminal_and_depots.indexOf(n1.id) == -1) {
+            let n1_nn = n1_nns[0];
+            if (this.nodes_index[n1_nn.id]) {
+              node_ids_to_delete.push(n1.id);
+              nn.path.push(n1.id);
+              nn.distance += n1_nn.distance;
+              nn.id = n1_nn.id;
+              n1 = this.nodes_index[n1_nn.id];
+              n1_nns = n1.next_nodes.filter(el => el.id != node.id && nn.path.indexOf(el.id) == -1);
+            }
+          }
+        }
+      }
+      let nodes_index_new = [];
+      let new_node_ids = [];
+      for (let node_id in this.nodes_index) {
+        let node = this.nodes_index[node_id];
+        if (node_ids_to_delete.indexOf(node.id) == -1) {
+          new_node_ids.push(node_id);
+          nodes_index_new[node_id] = this.nodes_index[node_id];
+        }
+      }
+      console.log('simplified step 1:', node_ids.length);
+      this.nodes_index = nodes_index_new;
+      console.log('simplified step 2:', new_node_ids.length);
+      console.log('simplified');*/
       console.log('nodes_index', this.nodes_index);
       this.ready_state = true;
-      let aws_nodes = [];
+      /*let aws_nodes = [];
       for (let node_id in this.nodes_index) {
         let node = this.nodes_index[node_id];
         aws_nodes.push({
@@ -330,10 +376,10 @@ export class MapComponent implements OnInit {
           lon: node.lon,
           next_nodes: node.next_nodes,
         })
-      }
+      }*/
       //console.log('nodes_index', JSON.stringify(this.nodes_index));
-      let graph_name_file = String(new Date().valueOf()) + '_' + this.file.name;
-      this.graph_name = graph_name_file + '.zip';
+      //let graph_name_file = String(new Date().valueOf()) + '_' + this.file.name;
+      //this.graph_name = graph_name_file + '.zip';
       this.is_loading -= 1;
       //this.http.post('https://6igj3zibqanqrt76fn6smiarhq0sopnv.lambda-url.eu-west-2.on.aws/',{graph_name:this.file.name,content:this.nodes},{responseType:'text'}).subscribe(data=>{
       /*this.http.post('https://6igj3zibqanqrt76fn6smiarhq0sopnv.lambda-url.eu-west-2.on.aws/', { graph_name: this.graph_name }).subscribe((data: any) => {
@@ -389,64 +435,110 @@ export class MapComponent implements OnInit {
   ready_state = false;
   idr = 0;
   runForAll() {
-    this.idr =0;
+    this.idr = 0;
     for (let dr of this.desired_routes) {
       if (dr.distance < this.max_distance) {
         this.idr += 1;
-        this.routeA(dr.start_node, dr.end_node);
-        console.log(dr.distance, this.max_distance, this.idr, 'routeA => ', dr.start_node, dr.end_node);
+        this.routeA(dr, dr.start_node, dr.end_node, dr.distance).then();
+        //console.log(dr.distance, this.max_distance, this.idr, 'routeA => ', dr.start_node, dr.end_node);
       }
     }
   }
-  routeA(from_node, to_node) {
-    console.log(' => routeA', from_node, to_node);
+  async routeA(dro, from_node, to_node, dist) {
+    console.log(' => routeA', dro, from_node, to_node);
     this.is_loading += 1;
     let new_queue: Route[] = [];
     let found = false;
+    let max_steps = 300 * dist;
     for (let nn of this.nodes_index[from_node].next_nodes) {
       this.min_dist_to_nodes[nn.id] = nn.distance;
       this.min_time_to_nodes[nn.id] = (nn.distance / 40) * 60;
       let n_speed = (nn.tags['maxspeed']) ? parseInt(nn.tags['maxspeed']) : this.default_speed;
-      new_queue.push({
-        nodes: [from_node, nn.id],
-        distance: nn.distance,
-        time: (nn.distance / n_speed) * 60,
-        time_to_end: turf.distance([this.nodes_index[nn.id].lon, this.nodes_index[nn.id].lat], [this.nodes_index[to_node].lon, this.nodes_index[to_node].lat]) / 40 * 60
-        //time_to_end: Math.sqrt(Math.pow(this.nodes_index[nn.id].lon * 100 - this.nodes_index[to_node].lon * 100, 2) + Math.pow(this.nodes_index[nn.id].lat * 100 - this.nodes_index[to_node].lat * 100, 2))
-      });
-      new_queue.sort((a, b) => { return (a.time + a.time_to_end > b.time + b.time_to_end) ? 1 : -1 });
+      if (this.nodes_index[nn.id])
+        new_queue.push({
+          nodes: [from_node, nn.id],
+          distance: nn.distance,
+          time: (nn.distance / n_speed) * 60,
+          time_to_end: turf.distance([this.nodes_index[nn.id].lon, this.nodes_index[nn.id].lat], [this.nodes_index[to_node].lon, this.nodes_index[to_node].lat]) / 40 * 60
+          //time_to_end: Math.sqrt(Math.pow(this.nodes_index[nn.id].lon * 100 - this.nodes_index[to_node].lon * 100, 2) + Math.pow(this.nodes_index[nn.id].lat * 100 - this.nodes_index[to_node].lat * 100, 2))
+        });
       //console.log(new_queue);
     }
+    new_queue.sort((a, b) => { return (a.time + a.time_to_end > b.time + b.time_to_end) ? 1 : -1 });
     let steps = 0;
-    while (!found && steps < 10000 && new_queue[0].distance < this.max_distance) {
+    while (!found && steps < max_steps && new_queue[0] !== undefined && new_queue[0].distance < this.max_distance) {
       steps += 1;
-      let queue0 = new_queue[0];
-      for (let nn of this.nodes_index[queue0.nodes[queue0.nodes.length - 1]].next_nodes) {
-        this.min_dist_to_nodes[nn.id] = nn.distance;
-        this.min_time_to_nodes[nn.id] = (nn.distance / 40) * 60;
-        let n_speed = (nn.tags['maxspeed']) ? parseInt(nn.tags['maxspeed']) : this.default_speed;
-        if (queue0.nodes.indexOf(nn.id) === -1)
-          new_queue.push({
-            nodes: [...queue0.nodes, nn.id],
-            distance: queue0.distance + nn.distance,
-            time: queue0.time + (nn.distance / n_speed) * 60,
-            //time_to_end: turf.distance([this.nodes_index[nn.id].lon, this.nodes_index[nn.id].lat], [this.nodes_index[to_node].lon, this.nodes_index[to_node].lat]) * 60
-            time_to_end: 10*Math.sqrt(Math.pow(this.nodes_index[nn.id].lon * 100 - this.nodes_index[to_node].lon * 100, 2) + Math.pow(this.nodes_index[nn.id].lat * 100 - this.nodes_index[to_node].lat * 100, 2))
-          });
+      //let q_items = new_queue.slice(0,Math.floor(new_queue.length*.2)+1);
+      let q_items = new_queue.slice(0, 1);
+      for (let q_item of q_items) {
+        //let q_item = new_queue[0];
+        for (let nn of this.nodes_index[q_item.nodes[q_item.nodes.length - 1]].next_nodes) {
+          this.min_dist_to_nodes[nn.id] = nn.distance;
+          this.min_time_to_nodes[nn.id] = (nn.distance / 40) * 60;
+          if (q_item.nodes.indexOf(nn.id) === -1 && this.nodes_index[nn.id]) {
+            let last_node_id = nn.id;
+            let acc_nodes = [nn.id];
+            //this.nodes_index[nn.id].on_map.bindTooltip([...q_item.nodes, ...acc_nodes].slice(-10).join('<br>')).addTo(this.map);
+            let acc_distance = q_item.distance + nn.distance;
+            let n_speed = (nn.tags['maxspeed']) ? parseInt(nn.tags['maxspeed']) : this.default_speed;
+            let acc_time = q_item.time + (nn.distance / n_speed) * 60;
+            let nn0 = this.nodes_index[nn.id].next_nodes;
+            while (nn0.filter(el => [...q_item.nodes, ...acc_nodes].indexOf(el.id) === -1).length == 1) {
+              if (nn.id == to_node) break;
+              let nn1 = nn0.filter(el => [...q_item.nodes, ...acc_nodes].indexOf(el.id) === -1)[0];
+              if (this.nodes_index[nn1.id] === undefined) break;
+              last_node_id = nn1.id;
+              acc_nodes.push(nn1.id);
+              //this.nodes_index[nn1.id].on_map.bindTooltip([...q_item.nodes, ...acc_nodes].slice(-10).join('<br>')).addTo(this.map);
+              acc_distance += nn1.distance;
+              n_speed = (nn1.tags['maxspeed']) ? parseInt(nn1.tags['maxspeed']) : this.default_speed;
+              acc_time += (nn1.distance / n_speed) * 60;
+              nn0 = this.nodes_index[nn1.id].next_nodes;
+              if (nn1.id == to_node) break;
+              if (acc_distance > this.max_distance) break;
+            }
+            let new_queue_item = {
+              nodes: [...q_item.nodes, ...acc_nodes],
+              distance: acc_distance,
+              time: acc_time,
+              time_to_end: turf.distance([this.nodes_index[last_node_id].lon, this.nodes_index[last_node_id].lat], [this.nodes_index[to_node].lon, this.nodes_index[to_node].lat]) / 40 * 60
+            };
+            if (last_node_id == to_node) {
+              found = true;
+              //let drf = this.desired_routes.find(el => el.start_node == from_node && el.end_node == to_node);
+              dro.distance = Math.round(new_queue_item.distance * 1000) / 1000;
+              dro.time = Math.round(new_queue_item.time);
+              dro.nodes = new_queue_item.nodes;
+              console.log(' => found', dro, from_node, to_node, max_steps, steps, new_queue_item);
+              break;
+            }
+            new_queue.push(new_queue_item);
+          }
+        }
       }
-      new_queue.splice(0, 1);
+      new_queue = new_queue.slice(q_items.length);
       new_queue.sort((a, b) => { return (a.time + a.time_to_end > b.time + b.time_to_end) ? 1 : -1 });
-      if (new_queue[0].nodes[new_queue[0].nodes.length - 1] == to_node) {
+      /*if (new_queue[0] && new_queue[0].nodes[new_queue[0].nodes.length - 1] == to_node) {
         found = true;
         this.desired_routes.find(el => el.start_node == from_node && el.end_node == to_node).distance = Math.round(new_queue[0].distance * 1000) / 1000;
         this.desired_routes.find(el => el.start_node == from_node && el.end_node == to_node).time = Math.round(new_queue[0].time);
         this.desired_routes.find(el => el.start_node == from_node && el.end_node == to_node).nodes = new_queue[0].nodes;
-        console.log(' => found', from_node, to_node, new_queue[0]);
-      }
-      //if (steps % 100 == 0) console.log(steps, new_queue[0]);
+        console.log(' => found', from_node, to_node, max_steps, steps, new_queue[0]);
+      }*/
     }
+    console.log(steps, max_steps, dro);
     //new_queue = undefined;
     this.is_loading -= 1;
+    if (this.is_loading == 0) {
+      for (let dr of this.desired_routes) {
+        let return_dr = this.desired_routes.find(el=>el.end_node==dr.start_node && el.start_node == dr.end_node);
+        if (return_dr && return_dr.time == null && dr.time) {
+          return_dr.time = dr.time;
+          return_dr.distance = dr.distance;
+        }
+      }
+    }
+    //console.log('desired_routes', this.desired_routes);
   }
   toggleRouteOnMap(desired_route) {
     if (desired_route.on_map === undefined) {
